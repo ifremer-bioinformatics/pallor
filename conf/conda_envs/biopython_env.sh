3be28c75bafb1c0eb04b2c6899c2ba5ac404b380
Alexandre Cormier <alexandre.cormier@ifremer.fr>
2020-06-11 15:52:47 +0200

    add env

---
+++ conf/conda_envs/biopython_env.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+. /appli/anaconda/3.7/etc/profile.d/conda.sh
+conda activate /appli/conda-env/bioinfo/conda-generic
---
+++ conf/conda_envs/iq-tree_env.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+. /appli/anaconda/3.7/etc/profile.d/conda.sh
+conda activate /appli/conda-env/bioinfo/iq-tree-2.0.3
