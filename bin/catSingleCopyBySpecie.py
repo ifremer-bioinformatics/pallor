0820b3c9d52b5f94153f37e356877c43c07f27c6
Alexandre Cormier <alexandre.cormier@ifremer.fr>
2022-06-13 06:58:48 +0000

    update

--- bin/catSingleCopyBySpecie.py
+++ bin/catSingleCopyBySpecie.py
@@ -1,30 +1,34 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse
 import os
 
+
 def getArgs():
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument('-f',dest="folder",type=str,required=True,help='Single copy BUSCO sequences folder')
-    parser.add_argument('-s',dest="specie",type=str,required=True,help='Specie ID')
+    parser.add_argument('-f', dest="folder", type=str, required=True, help='Single copy BUSCO sequences folder')
+    parser.add_argument('-d', dest="dbodb", type=str, required=True, help='BUSCO ODB_10 database name')
+    parser.add_argument('-s', dest="specie", type=str, required=True, help='Specie ID')
 
     arg = parser.parse_args()
 
     return arg
 
-def main(args):
 
-    cat_faa_sg = open(args.specie+'.sg.faa', 'w')
-    for file in os.listdir(args.folder):
-        if file.endswith(".faa"):
-            faa = open(os.path.join(args.folder, file), 'r')
+def main(args):
+    cat_faa_sg = open(args.specie + '.sg.faa', 'w')
+    busco_single_copy_folder = os.path.join(args.folder, 'run_' + args.dbodb, 'busco_sequences/single_copy_busco_sequences')
+    for file in os.listdir(busco_single_copy_folder):
+        if file.endswith('.faa'):
+            faa = open(os.path.join(busco_single_copy_folder, file), 'r')
             odb_id = os.path.splitext(file)[0]
             for line in faa:
                 if line.startswith('>'):
-                    ctg_start_end = line.split()[0].replace('>','')
-                    cat_faa_sg.write('>'+odb_id+':'+args.specie+':'+ctg_start_end+'\n')
+                    ctg_start_end = line.split()[0].replace('>', '')
+                    cat_faa_sg.write('>' + odb_id + ':' + args.specie + ':' + ctg_start_end + '\n')
                 else:
                     cat_faa_sg.write(line)
+
 
 if __name__ == '__main__':
     args = getArgs()
--- bin/extractSharedSingleCopy.py
+++ bin/extractSharedSingleCopy.py
@@ -3,38 +3,41 @@
 import argparse
 from Bio import SeqIO
 
+
 def getArgs():
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument('-f',dest="fasta", type=str,required=True,help='Fasta file with all single copy busco sequences')
-    parser.add_argument('-s',dest="species",type=int,required=True,help='Minimal number of species per cluster')
+    parser.add_argument('-f', dest="fasta", type=str, required=True,
+                        help='Fasta file with all single copy busco sequences')
+    parser.add_argument('-s', dest="species", type=int, required=True, help='Minimal number of species per cluster')
 
     args = parser.parse_args()
 
     return args
 
-def main(args):
 
+def main(args):
     sg_odb = {}
 
     # 1 - Store each sequence of each specie by orthologs
     for fasta in SeqIO.parse(args.fasta, "fasta"):
         og_id, specie, contig, coordinates = fasta.id.split(':')
         if not og_id in sg_odb:
-            sg_odb[og_id] = { specie: [fasta.id, fasta.seq]}
+            sg_odb[og_id] = {specie: [fasta.id, fasta.seq]}
         else:
             sg_odb[og_id][specie] = [fasta.id, fasta.seq]
 
     # 2 - Pass on each ortholgous sequences and write if shared by a minimal number of species
     for og_id, species in sg_odb.items():
         if len(species) >= args.species:
-            og_file = open(og_id+'.faa','w')
+            og_file = open(og_id + '.faa', 'w')
             for specie in species:
                 # header = species[specie][0]
                 seq = str(species[specie][1])
-                og_file.write('>'+specie+'\n')
+                og_file.write('>' + specie + '\n')
                 while len(seq) > 0:
-                    og_file.write(seq[:70]+'\n')
+                    og_file.write(seq[:70] + '\n')
                     seq = seq[70:]
+
 
 if __name__ == '__main__':
     args = getArgs()
